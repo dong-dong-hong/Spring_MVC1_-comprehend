92a70c68ccd30bd556dac8a9db05a588a4b7b763
동동 <87763201+dong-dong-hong@users.noreply.github.com>
2023-03-21 00:26:06 +0900

    commit

---
+++ src/main/java/hello/servlet/web/frontController/v2/FontControllerServletV2.java
@@ -0,0 +1,47 @@
+package hello.servlet.web.frontController.v2;
+
+
+import hello.servlet.web.frontController.MyView;
+import hello.servlet.web.frontController.v2.controller.MemberFormControllerV2;
+import hello.servlet.web.frontController.v2.controller.MemberListControllerV2;
+import hello.servlet.web.frontController.v2.controller.MemberSaveControllerV2;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+// /front-controller/v2/members/new-form
+@WebServlet(name="fontControllerServletV2", urlPatterns = "/front-controller/v2/*") // /front-controller/v1를 포함한 하위 모든 요청은 이 서블릿에서 받아들인다.
+                                                                                    // 예) /front-controller/v1/a ,  /front-controller/v1/a/b
+public class FontControllerServletV2 extends HttpServlet {
+
+    private Map<String, ControllerV2> controllerMap = new HashMap<>();
+
+    public FontControllerServletV2() {
+        controllerMap.put("/front-controller/v2/members/new-form", new MemberFormControllerV2());
+        controllerMap.put("/front-controller/v2/members/save", new MemberSaveControllerV2());
+        controllerMap.put("/front-controller/v2/members", new MemberListControllerV2());
+        // controllerMap
+        // key : 매핑 URL
+        // value : 호출될 컨트롤러
+    }
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String requestURI = request.getRequestURI();
+
+        ControllerV2 controller = controllerMap.get(requestURI);
+        if(controller == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        MyView view = controller.process(request, response); // ctrl + alt + B -> 구현체를 볼 수 있는 단축키
+        view.render(request,response);
+    }
+}
