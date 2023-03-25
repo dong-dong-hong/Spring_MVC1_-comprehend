e81b37771559c90e0a918be469f0676c7087f17b
동동 <87763201+dong-dong-hong@users.noreply.github.com>
2023-03-25 23:22:51 +0900

    commit

--- src/main/java/hello/servlet/web/frontController/v5/adapter/ControllerV3HandlerAdapter.java
+++ src/main/java/hello/servlet/web/frontController/v5/adapter/ControllerV3HandlerAdapter.java
@@ -1,21 +1,32 @@
 package hello.servlet.web.frontController.v5.adapter;
 
 import hello.servlet.web.frontController.ModelView;
+import hello.servlet.web.frontController.v3.ControllerV3;
 import hello.servlet.web.frontController.v5.MyHandlerAdapter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
     @Override
     public boolean support(Object handler) {
-        return false;
+        return(handler instanceof ControllerV3);
     }
 
     @Override
-    public ModelView handle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        return null;
+    public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
+        // MemberFormControllerV3
+        ControllerV3 controller = (ControllerV3) handler;
+        Map<String, String> paramMap = createParamMap(request);
+        return controller.process(paramMap);
+    }
+    private Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String,String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator().forEachRemaining(paramName -> paramMap.put(paramName,request.getParameter(paramName)));
+        return paramMap;
     }
 }
